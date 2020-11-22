b318348a13abef6945eac69d10dfa6329dcfcc71
nebula1989 <benwalterscoding@gmail.com>
2020-11-22 16:37:32 -0500

    setup bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
-from flask import request
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return render_template('under-construction.html')
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
