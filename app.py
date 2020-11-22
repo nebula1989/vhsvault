6666b6d3d94139891691c126ce09c7da9d28f239
nebula1989 <benwalterscoding@gmail.com>
2020-11-22 12:26:02 -0500

    basic starting point

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to VHSVault.  This page is currently under construction.'
+def index():
+    return render_template('under-construction.html')
 
 
-@app.route('/<name>')
+@app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}! </h1>'.format(name)
 
