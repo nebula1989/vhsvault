b80018664674ab80c665dd32f5e3297232d7e02d
nebula1989 <benwalterscoding@gmail.com>
2020-12-13 16:31:54 -0500

    sqlite setup

--- app.py
+++ app.py
@@ -1,17 +1,73 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, UserManager, UserMixin
 
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}! </h1>'.format(name)
+# Class-based application configuration
+class ConfigClass(object):
+    """ Flask application config """
+
+    # Flask settings
+    SECRET_KEY = "25297929ac6f4ea4bf967dd8a5b111bd"
+
+    # Flask-SQLAlchemy settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///vhsvault.sqlite'    # File-based SQL database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
+
+    # Flask-User settings
+    USER_APP_NAME = "VhsVault"      # Shown in and email templates and page footers
+    USER_ENABLE_EMAIL = False      # Disable email authentication
+    USER_ENABLE_USERNAME = True    # Enable username authentication
+    USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
 
 
+def create_app():
+    """ Flask application factory """
+
+    # Create Flask app load app.config
+    app = Flask(__name__)
+    app.config.from_object(__name__ + '.ConfigClass')
+
+    # Initialize Flask-SQLAlchemy
+    db = SQLAlchemy(app)
+
+    # Define the User data-model.
+    # NB: Make sure to add flask_user UserMixin !!!
+    class User(db.Model, UserMixin):
+        __tablename__ = 'users'
+        id = db.Column(db.Integer, primary_key=True)
+        active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+        # User authentication information. The collation='NOCASE' is required
+        # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+        username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
+        password = db.Column(db.String(255), nullable=False, server_default='')
+        email_confirmed_at = db.Column(db.DateTime())
+
+        # User information
+        first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+        last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+
+    # Create all database tables
+    db.create_all()
+
+    # Setup Flask-User and specify the User data-model
+    user_manager = UserManager(app, db, User)
+
+    @app.route('/')
+    def home_page():
+        return render_template('index.html')
+
+    # The Members page is only accessible to authenticated users via the @login_required decorator
+    @app.route('/members')
+    @login_required    # User must be authenticated
+    def member_page():
+        # String-based templates
+        return render_template('member-page.html')
+
+    return app
+
+
+# Start development web server
 if __name__ == '__main__':
-    app.run()
+    app = create_app()
