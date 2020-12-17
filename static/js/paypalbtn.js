613501652dc32438dc4970f3dac3d304da0394fe
nebula1989 <benwalterscoding@gmail.com>
2020-12-16 20:16:51 -0500

    updated paymentbutton

--- static/js/paypalbtn.js
+++ static/js/paypalbtn.js
@@ -1,28 +1,28 @@
-function initPayPalButton() {
-              paypal.Buttons({
-                style: {
-                  shape: 'rect',
-                  color: 'gold',
-                  layout: 'vertical',
-                  label: 'paypal',
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'pill',
+          color: 'gold',
+          layout: 'horizontal',
+          label: 'paypal',
+          
+        },
 
-                },
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+          });
+        },
 
-                createOrder: function(data, actions) {
-                  return actions.order.create({
-                    purchase_units: [{"description":"If you enjoy this site, consider making a donation to help support the costs of web services.","amount":{"currency_code":"USD","value":1}}]
-                  });
-                },
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(details) {
+            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+          });
+        },
 
-                onApprove: function(data, actions) {
-                  return actions.order.capture().then(function(details) {
-                    alert('Transaction completed by ' + details.payer.name.given_name + '!');
-                  });
-                },
-
-                onError: function(err) {
-                  console.log(err);
-                }
-              }).render('#paypal-button-container');
-            }
-            initPayPalButton();
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+    initPayPalButton();
