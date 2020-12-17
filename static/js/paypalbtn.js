3bbc2ef267fcbefc59dfc649227c765c7f053933
nebula1989 <benwalterscoding@gmail.com>
2020-12-17 07:41:10 -0500

    fix paypal btn

--- static/js/paypalbtn.js
+++ static/js/paypalbtn.js
@@ -1,16 +1,17 @@
+
     function initPayPalButton() {
       paypal.Buttons({
         style: {
           shape: 'pill',
           color: 'gold',
-          layout: 'horizontal',
+          layout: 'vertical',
           label: 'paypal',
           
         },
 
         createOrder: function(data, actions) {
           return actions.order.create({
-            purchase_units: [{"amount":{"currency_code":"USD","value":1}}]
+            purchase_units: [{"description":"Tip Jar","amount":{"currency_code":"USD","value":1}}]
           });
         },
 
