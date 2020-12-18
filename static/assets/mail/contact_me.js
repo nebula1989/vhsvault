003d4f5f768e7fae40ca30e9e214acab3b8dbdea
nebula1989 <benwalterscoding@gmail.com>
2020-12-18 12:30:04 -0500

    mail still doesn't work, now smoothness doesn't work again

--- static/assets/mail/contact_me.js
+++ static/assets/mail/contact_me.js
@@ -9,16 +9,16 @@ $(function () {
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            const name = $("input#name").val();
+            const email = $("input#email").val();
+            const phone = $("input#phone").val();
+            const message = $("textarea#message").val();
+            let firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(" ") >= 0) {
                 firstName = name.split(" ").slice(0, -1).join(" ");
             }
-            $this = $("#sendMessageButton");
+            let $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
                 url: "/static/assets/mail/contact_me.php",
