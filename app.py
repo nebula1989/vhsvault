884adb09751461f166bf4b1af17827bedf0f4f84
nebula1989 <benwalterscoding@gmail.com>
2020-12-20 17:01:28 -0500

    displays youtube videos dynamically

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import get_embed_youtube
 
 app = Flask(__name__)
 
@@ -8,9 +9,16 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/videos')
+def video():
+    return render_template('videos.html', video_ids=get_embed_youtube.video_ids)
+
+
+"""
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}! </h1>'.format(name)
+"""
 
 
 if __name__ == '__main__':
