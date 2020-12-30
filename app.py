b62500c4ac2b71e7ed69eaa67aa5512e584e8030
nebula1989 <benwalterscoding@gmail.com>
2020-12-30 09:57:12 -0500

    fix merge issue

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
-import get_embed_youtube
+import get_yt__playlist_data
 
 
 app = Flask(__name__)
+app.jinja_env.filters['zip'] = zip
 
 
 @app.route('/')
@@ -12,7 +13,7 @@ def index():
 
 @app.route('/videos')
 def video():
-    return render_template('videos.html', video_ids=get_embed_youtube.video_ids)
+    return render_template('videos.html', playlist_data=get_yt__playlist_data.yt_playlist_data, playlist_ids=get_yt__playlist_data.yt_playlist_ids, playlist_length=get_yt__playlist_data.amount_of_playlists)
 
 
 """
--- get_yt__playlist_data.py
+++ get_yt__playlist_data.py
@@ -1,21 +1,44 @@
 import os
 import googleapiclient.discovery
 import googleapiclient.errors
+from pprint import pprint
 
 
-def main():
-    api_service_name = "youtube"
-    api_version = "v3"
-    api_key = os.environ.get('YT_API_KEY')
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
-    request = youtube.playlists().list(part="snippet,contentDetails", channelId="UCbjjRr-zfV6st2J_LBOhoHw", maxResults=25)
-    response = request.execute()
+def get_playlist_data(response_dict):
+    # playlist data dictionary : [id, title, description]
 
-    titles = get_playlist_titles(response)
-    descriptions = get_playlist_descriptions(response)
+    playlist_data_dict_list = response_dict['items'] # get items
+    amount = response_dict['pageInfo']['totalResults'] # get total results
 
-    display(titles, descriptions)
+    return playlist_data_dict_list, amount
 
+
+def playlist_ids(response_dict, num):
+    playlist_ids_list = []
+
+    for playlist in range(num):
+        playlist_ids_list.append(response_dict[playlist]['id']) # add each playlist add to array
+
+    return playlist_ids_list
+
+
+def display(data):
+    pprint(data)
+
+
+api_service_name = "youtube"
+api_version = "v3"
+api_key = os.environ.get('YT_API_KEY')
+youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+request = youtube.playlists().list(part="id,snippet,contentDetails", channelId="UCbjjRr-zfV6st2J_LBOhoHw", maxResults=50)
+response = request.execute()
+
+yt_playlist_data, amount_of_playlists = get_playlist_data(response)
+yt_playlist_ids = playlist_ids(yt_playlist_data, amount_of_playlists)
+
+
+
+"""
 
 def get_playlist_titles(response_dict):
     playlist_titles = []
@@ -32,11 +55,4 @@ def get_playlist_descriptions(response_dict):
 
     return playlist_descriptions
 
-
-def display(t, d):
-    print(t)
-    print(d)
-
-
-if __name__ == '__main__':
-    main()
+"""
